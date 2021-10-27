830aa516cffecbf4661d00b3e00af1b8bbe5d808
Thiago Veronezi <thiago@veronezi.org>
2021-10-27 15:05:43 -0400

    testing the cli in an integration test

--- inclusion-service/tests/crud.rs
+++ inclusion-service/tests/crud.rs
@@ -1,8 +1,29 @@
 mod common;
 
+use assert_cmd::prelude::*;
+use std::process::Command;
+use std::{thread, time};
+
 #[test]
 fn test_crud() {
     let client = common::Client::default();
     let stack = client.new_stack();
-    assert!(stack.articlesdb_url().starts_with("postgres://rusty:rusty"))
+    let db_url = stack.articlesdb_url();
+    let bind = format!(
+        "127.0.0.1:{}",
+        portpicker::pick_unused_port().expect("No ports free")
+    );
+    let bind_thread = bind.clone();
+    thread::spawn(move || {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.env("articles_db_url", db_url);
+        cmd.env("bind", bind_thread);
+        let _ = cmd.unwrap();
+    });
+    thread::sleep(time::Duration::from_secs(2));
+    let resp = reqwest::blocking::get(format!("http://{}/api/v1/system/ping", bind))
+        .unwrap()
+        .text()
+        .unwrap();
+    assert_eq!("pong", resp);
 }
